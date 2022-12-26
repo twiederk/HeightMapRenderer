3c38e6d18694b998461b40a15a539c1280971fea
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-26 19:24:58 +0100

    Read height map from file

--- HeightMapRenderer.gd
+++ HeightMapRenderer.gd
@@ -1,30 +1,17 @@
 class_name HeighMapRenderer
 extends Node2D
 
-
-#aabqponm
-#abcryxxl
-#accszzxk
-#acctuvwj
-#abdefghi
-
-var row1 = "aabqponm"
-var row2 = "abcryxxl"
-var row3 = "accszzxk"
-var row4 = "acctuvwj"
-var row5 = "abdefghi"
-var height_map = [row1, row2, row3, row4, row5]
-
 var colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 
 onready var Map = $TileMap
 
 func _ready():
-	make_height_map()
+	var height_map = load_file("res://assets/Day12_TestData.tres")
+	make_height_map(height_map)
 
 
-func make_height_map() -> void:
+func make_height_map(height_map) -> void:
 	Map.clear()
 	for row in range(height_map.size()):
 		for col in range(height_map[0].length()):
@@ -35,3 +22,14 @@ func make_height_map() -> void:
 
 func get_height(height :int) -> int:
 	return height - ord("a")
+
+
+func load_file(fileName):
+	var height_map = []
+	var file = File.new()
+	file.open(fileName, File.READ)
+	while not file.eof_reached():
+		height_map.append(file.get_line())
+	file.close()
+	height_map.remove(height_map.size() - 1)
+	return height_map
