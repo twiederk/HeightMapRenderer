2f9746a90db2bba35ed86aa314b375f69229b560
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-26 18:54:49 +0100

    Render test data as height map

--- World.gd
+++ World.gd
@@ -1,32 +1,35 @@
 extends Node2D
 
-var width = 20  # width of map (in tiles)
-var height = 12  # height of map (in tiles)
+#aabqponm
+#abcryxxl
+#accszzxk
+#acctuvwj
+#abdefghi
 
-var row1 = [0, 1, 2, 3, 4]
-var row2 = [5, 6, 7, 8, 9]
-var height_map = [row1, row2]
-
+var row1 = "aabqponm"
+var row2 = "abcryxxl"
+var row3 = "accszzxk"
+var row4 = "acctuvwj"
+var row5 = "abdefghi"
+var height_map = [row1, row2, row3, row4, row5]
 
 var colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 
-# get a reference to the map for convenience
 onready var Map = $TileMap
 
 func _ready():
-#	randomize()
-#	tile_size = Map.cell_size
-	make_maze()
+	make_height_map()
 
 
-func make_maze():
-#	var unvisited = []  # array of unvisited tiles
-#	var stack = []
-	# fill the map with solid tiles
+func make_height_map():
 	Map.clear()
-	for x in range(height_map[0].size()):
-		for y in range(height_map.size()):
-#			unvisited.append(Vector2(x, y))
-			var height = height_map[y][x]
-			Map.set_cellv(Vector2(x, y), colors[height])
+	for row in range(height_map.size()):
+		for col in range(height_map[0].length()):
+			var height: int = get_height(height_map[row].ord_at(col))
+			var scaled_height = height / 2
+			Map.set_cellv(Vector2(col, row), colors[scaled_height])
+
+
+func get_height(height :int) -> int:
+	return height - ord("a")
