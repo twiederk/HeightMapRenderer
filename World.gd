20fd7eb71d4ba254ce865c8fcdf990c80b1874c2
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-26 18:06:30 +0100

    Read color from color array

--- World.gd
+++ World.gd
@@ -3,11 +3,13 @@ extends Node2D
 var width = 20  # width of map (in tiles)
 var height = 12  # height of map (in tiles)
 
-
 var row1 = [0, 1, 2, 3, 4]
 var row2 = [5, 6, 7, 8, 9]
-
 var height_map = [row1, row2]
+
+
+var colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 
 # get a reference to the map for convenience
 onready var Map = $TileMap
@@ -26,4 +28,5 @@ func make_maze():
 	for x in range(height_map[0].size()):
 		for y in range(height_map.size()):
 #			unvisited.append(Vector2(x, y))
-			Map.set_cellv(Vector2(x, y), height_map[y][x])
+			var height = height_map[y][x]
+			Map.set_cellv(Vector2(x, y), colors[height])
